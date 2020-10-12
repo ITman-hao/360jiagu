2199266656ea2c311987c57267eea45162263b04
caojunhao <1547381966@qq.com>
2020-10-12 22:20:17 +0800

    Initial commit

---
+++ plugin/src/main/java/com/example/cjh/plugin/ConfigModel.java
@@ -0,0 +1,70 @@
+package com.example.cjh.plugin;
+
+/**
+ * 用来配置用户的信息
+ */
+public class ConfigModel {
+    private String userName;
+    private String userPassword;
+    private String keyStorePath;
+    private String keyStorePassword;
+    private String keyAlias;
+    private String keyAliasPassword;
+    private String jarToolsPath;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void setKeyStorePath(String keyStorePath) {
+        this.keyStorePath = keyStorePath;
+    }
+
+    public void setKeyStorePassword(String keyStorePassword) {
+        this.keyStorePassword = keyStorePassword;
+    }
+
+    public void setKeyAlias(String keyAlias) {
+        this.keyAlias = keyAlias;
+    }
+
+    public void setKeyAliasPassword(String keyAliasPassword) {
+        this.keyAliasPassword = keyAliasPassword;
+    }
+
+    public void setJarToolsPath(String jarToolsPath) {
+        this.jarToolsPath = jarToolsPath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getKeyStorePath() {
+        return keyStorePath;
+    }
+
+    public String getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public String getKeyAlias() {
+        return keyAlias;
+    }
+
+    public String getKeyAliasPassword() {
+        return keyAliasPassword;
+    }
+
+    public String getJarToolsPath() {
+        return jarToolsPath;
+    }
+}
